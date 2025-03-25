53f760e5d74daa8d97343f36b80591b2507671c5
hoangtien <hoangtien19516@gmail.com>
2025-03-25 15:28:03 +0700

    commit init

---
+++ CAG_CHANGE_LINE/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CAG_CHANGE_LINE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grb_TTM = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // grb_TTM
+            // 
+            this.grb_TTM.Location = new System.Drawing.Point(12, 66);
+            this.grb_TTM.Name = "grb_TTM";
+            this.grb_TTM.Size = new System.Drawing.Size(379, 119);
+            this.grb_TTM.TabIndex = 0;
+            this.grb_TTM.TabStop = false;
+            this.grb_TTM.Text = "Thông Tin Mạng";
+            this.grb_TTM.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(397, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(391, 85);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox1";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grb_TTM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grb_TTM;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
